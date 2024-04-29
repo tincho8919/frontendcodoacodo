9a7e3a13538e7ea395c0629b355ea5fdc947cba9
martin <tincho8919@gmail.com>
2024-04-29 00:54:43 -0300

    header

---
+++ JS/index.js
@@ -0,0 +1,8 @@
+
+/* const ham = document.querySelector('.ham');
+const enlaces = document.querySelector('.nav');
+
+
+ham.addEventListener('click', () => {
+    enlaces.classList.toggle('activado');
+}); */

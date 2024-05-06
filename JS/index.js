07d723ee0a64d16ef25d88e6b0fd1d7b4189bbd7
martin <tincho8919@gmail.com>
2024-05-06 20:56:16 -0300

    escrituta onepage

--- JS/index.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function () {
-    const formulario = document.getElementById('miFormulario');
-
-    formulario.addEventListener('submit', function (event) {
-        event.preventDefault();
-        formulario.submit();
-    });
-});
